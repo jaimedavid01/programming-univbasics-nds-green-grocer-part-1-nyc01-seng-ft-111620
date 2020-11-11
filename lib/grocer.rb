845f793b147efd57bb86e51403bdafad9e82d1de
Jaime David <jaimedavid01@yahoo.com>
2020-11-11 08:23:24 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -24,27 +24,28 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   
+def consolidate_cart(cart)
+  index = 0
   new_cart = []
-  counter = 0 
-  while counter < cart.count 
-  new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-  if new_cart_item != nil
-    new_cart_item[:count] += 1
-  else
-    new_cart_item = {
-      :item => cart[counter][:item],
-      :price => cart[counter][:price],
-      :clearance => cart[counter][:clearnace],
-      :count => 1
-    }
-  new_cart << new_cart_item
-end
-counter += 1
-end
-end
   
-  
-
+  cart.each do |grocery_item|
+    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    if current_item
+      new_cart_index = 0
+      new_cart.each do |new_cart_item|
+        if new_cart_item[:item] === current_item[:item]
+          new_cart_item[:count] += 1
+        end
+        new_cart_index += 1
+      end
+    else
+      grocery_item[:count] = 1
+      new_cart << grocery_item
+    end
+    index += 1
+  end
+  new_cart
+end
 #   new_cart = {}
   
 # cart.each do |hashes|
